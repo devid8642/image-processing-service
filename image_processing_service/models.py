bd122f2a6443a71f18bba3aa96f307b04bf71ecd
Dêvid Teófilo <nnick4673@gmail.com>
2025-03-22 15:02:07 -0300

    feat: adicionar modelo de imagem e tabela correspondente no banco de dados

--- image_processing_service/models.py
+++ image_processing_service/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, func
+from sqlalchemy import DateTime, func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, registry
 
 table_registry = registry()
@@ -14,5 +14,18 @@ class User:
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
     created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), init=False, default=func.now()
+    )
+
+
+@table_registry.mapped_as_dataclass
+class Image:
+    __tablename__ = 'images'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    filename: Mapped[str]
+    url: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    uploaded_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), init=False, default=func.now()
     )
